e8cb03d7641b806539f6ba837693f3636f7eb31c
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2023-06-12 20:45:38 +0200

    start to build my own slider

--- pages/galery/index.js
+++ pages/galery/index.js
@@ -1,29 +1,10 @@
-import Lightbox from 'yet-another-react-lightbox';
-import 'yet-another-react-lightbox/styles.css';
-import { useState } from 'react';
-const images = [
-	{
-		src: 'images/konstancin-1.png',
-	},
-	{
-		src: 'images/konstancin-2.png',
-	},
-	{
-		src: 'images/konstancin-3.png',
-	},
-];
+import Slider from '@/components/AddTask.js/Slider.js/Slider';
 
 export default function Galery() {
-	const [open, setOpen] = useState(false);
-
 	return (
 		<>
-			<h2>galery</h2>
-			<button type='button' onClick={() => setOpen(true)}>
-				Open Lightbox
-			</button>
-
-			<Lightbox open={open} close={() => setOpen(false)} slides={images} />
+			<h2>Galery</h2>
+			<Slider />
 		</>
 	);
 }
